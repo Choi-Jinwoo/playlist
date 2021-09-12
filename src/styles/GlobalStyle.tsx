068854c3ccee28638552a45386fd18246eaad6bd
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-12 15:23:17 +0900

    add: Interop font 추가

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -3,6 +3,11 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+
+  body {
+    font-size: 3rem;
+    font-family: Interop, sans-serif;
+  }
 `;
 
 export default GlobalStyle;
