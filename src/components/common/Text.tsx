9d0e79fba0651ef0142c38473dcd974abd5dcfc6
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-12 16:50:03 +0900

    fix: Text 컴포넌트의 margin 속성 추가

--- src/components/common/Text.tsx
+++ src/components/common/Text.tsx
@@ -4,12 +4,22 @@ type Props = {
   size?: string;
   weight?: 'bold' | 'bolder' | 'light' | 'lighter';
   color?: string;
+  margin?: {
+    top?: number;
+    bottom?: number;
+    left?: number;
+    right?: number;
+  };
 }
 
-const Text = styled.span<Props>`
+const Text = styled.p<Props>`
   font-size: ${props => props.size ?? props.theme.fontSize.medium};
   font-weight: ${props => props.weight ?? 'normal'};
-  color: ${props => props.color ?? props.theme.color.black}
+  color: ${props => props.color ?? props.theme.color.black};
+  margin-top: ${props => props.margin?.top ?? 0}px;
+  margin-bottom: ${props => props.margin?.bottom ?? 0}px;
+  margin-left: ${props => props.margin?.left ?? 0}px;
+  margin-right: ${props => props.margin?.right ?? 0}px;
 `;
 
 export default Text;
