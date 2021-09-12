05547e5305b4c5b06c3c7b7cb35a80ea141a8f6a
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-12 14:57:21 +0900

    add: styled-compoents 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+import GlobalStyle from './styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 const App = (): JSX.Element => {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 };
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,22 @@
+const theme = {
+  deviceWidth: {
+  },
+  innerWidth: {
+    desktop: '1200px',
+  },
+  color: {
+    main1: '#93B5C6',
+    main2: '#C9CCD5',
+    main3: '#E4D8DC',
+    main4: '#FFE3E3',
+  },
+  fontSize: {
+    xLarge: '1.5rem',
+    large: '1.25rem',
+    medium: '1rem',
+    small: '0.75rem',
+    tiny: '0.5rem',
+  },
+};
+
+export default theme;
