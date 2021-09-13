af0d12e16fcc83fd6e36d9126d711912ed2bc102
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-13 20:09:54 +0900

    PlayListPage에서 Audio 관련 hooks를 통해 state 관리

--- src/pages/PlaylistPage.tsx
+++ src/pages/PlaylistPage.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PageTemplate from '../components/common/PageTemplate';
+import AudioControls from '../components/Playlist/AudioControls';
 import CDPlayer from '../components/Playlist/CDPlayer';
+import useAudioPause from '../hooks/useAudioPause';
+import useAudioTimeline from '../hooks/useAudioTimeline';
 
 const PlaylistPage = (): JSX.Element => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const [isPaused, handlePause, handlePlay] = useAudioPause(audioRef);
+  const [currentTime, duration, handleJumpTo] = useAudioTimeline(audioRef);
+
+  const handleAutoPaused = () => {
+    if (!isPaused) {
+      handlePause();
+    }
+  };
+
+  const handleAutoPlay = () => {
+    if (isPaused) {
+      handlePlay();
+    }
+  };
+
   return (
     <PageTemplate>
-      <CDPlayer thumbnail="https://img1.daumcdn.net/thumb/R1280x0.fpng/?fname=http://t1.daumcdn.net/brunch/service/user/8fXh/image/M7kpICX4bbcXfuMgX2DH0NT0qeg.png" />
+      <audio ref={audioRef} hidden={true} onPause={handleAutoPaused} onPlay={handleAutoPlay} >
+        <source src="/music/santa-tell-me.mp3" />
+      </audio>
+      <CDPlayer isPaused={isPaused} thumbnail="https://t1.daumcdn.net/cfile/tistory/2643F34C59242C8E26" />
+      <AudioControls
+        handleJumpTo={handleJumpTo}
+        currentTime={currentTime}
+        duration={duration}
+        handlePause={handlePause}
+        handlePlay={handlePlay}
+        isPaused={isPaused} />
     </PageTemplate>
   );
 };
