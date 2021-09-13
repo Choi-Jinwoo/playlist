5c7c1acaa7daaf34ea1ec13400b8fe2f935e139c
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-13 14:20:02 +0900

    fix: CDPlayer의 background clip을 padding으로 변경 및 hole 크기 수정

--- src/pages/PlaylistPage.tsx
+++ src/pages/PlaylistPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PageTemplate from '../components/common/PageTemplate';
+import CDPlayer from '../components/Playlist/CDPlayer';
 
 const PlaylistPage = (): JSX.Element => {
   return (
     <PageTemplate>
-      <span>PL page</span>
+      <CDPlayer thumbnail="https://img1.daumcdn.net/thumb/R1280x0.fpng/?fname=http://t1.daumcdn.net/brunch/service/user/8fXh/image/M7kpICX4bbcXfuMgX2DH0NT0qeg.png" />
     </PageTemplate>
   );
 };
