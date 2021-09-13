e1ae1c3f1cb6ff9297a3a5d4e0b1ac0189be5d49
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-13 20:51:23 +0900

    fix: PlaylistPage의 상태 관리를 Palyer 컴포넌트로 수정

---
+++ src/components/Playlist/Player/index.tsx
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react';
+import styled from 'styled-components';
+import useAudioPause from '../../../hooks/useAudioPause';
+import useAudioTimeline from '../../../hooks/useAudioTimeline';
+import AudioControls from './AudioControls';
+import CDPlayer from './CDPlayer';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Player = (): JSX.Element => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const [isPaused, handlePause, handlePlay] = useAudioPause(audioRef);
+  const [currentTime, duration, handleJumpTo] = useAudioTimeline(audioRef);
+
+  const handleAutoPaused = () => {
+    if (!isPaused) {
+      handlePause();
+    }
+  };
+
+  const handleAutoPlay = () => {
+    if (isPaused) {
+      handlePlay();
+    }
+  };
+
+  return (
+    <Container>
+      <audio ref={audioRef} hidden={true} onPause={handleAutoPaused} onPlay={handleAutoPlay} >
+        <source src="/music/santa-tell-me.mp3" />
+      </audio>
+      <CDPlayer isPaused={isPaused} thumbnail="https://t1.daumcdn.net/cfile/tistory/2643F34C59242C8E26" />
+      <AudioControls
+        handleJumpTo={handleJumpTo}
+        currentTime={currentTime}
+        duration={duration}
+        handlePause={handlePause}
+        handlePlay={handlePlay}
+        isPaused={isPaused} />
+    </Container>
+  );
+};
+
+export default Player;
--- src/pages/PlaylistPage.tsx
+++ src/pages/PlaylistPage.tsx
@@ -1,41 +1,11 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import PageTemplate from '../components/common/PageTemplate';
-import AudioControls from '../components/Playlist/AudioControls';
-import CDPlayer from '../components/Playlist/CDPlayer';
-import useAudioPause from '../hooks/useAudioPause';
-import useAudioTimeline from '../hooks/useAudioTimeline';
+import Player from '../components/Playlist/Player';
 
 const PlaylistPage = (): JSX.Element => {
-  const audioRef = useRef<HTMLAudioElement>(null);
-
-  const [isPaused, handlePause, handlePlay] = useAudioPause(audioRef);
-  const [currentTime, duration, handleJumpTo] = useAudioTimeline(audioRef);
-
-  const handleAutoPaused = () => {
-    if (!isPaused) {
-      handlePause();
-    }
-  };
-
-  const handleAutoPlay = () => {
-    if (isPaused) {
-      handlePlay();
-    }
-  };
-
   return (
     <PageTemplate>
-      <audio ref={audioRef} hidden={true} onPause={handleAutoPaused} onPlay={handleAutoPlay} >
-        <source src="/music/santa-tell-me.mp3" />
-      </audio>
-      <CDPlayer isPaused={isPaused} thumbnail="https://t1.daumcdn.net/cfile/tistory/2643F34C59242C8E26" />
-      <AudioControls
-        handleJumpTo={handleJumpTo}
-        currentTime={currentTime}
-        duration={duration}
-        handlePause={handlePause}
-        handlePlay={handlePlay}
-        isPaused={isPaused} />
+      <Player />
     </PageTemplate>
   );
 };
