b1d29e3a894b2978b120cdcedf9feb93241b8afd
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-14 10:55:40 +0900

    add: PlayerPage에 MusicList 컴포넌트 추가

--- src/hooks/useMusics.ts
+++ src/hooks/useMusics.ts
@@ -22,6 +22,34 @@ const useMusics = (): UseMusics => {
         thumbnail: 'http://news.imaeil.com/inc/photos/2019/05/03/2019050311475036152_l.jpg',
         audioSource: '',
       },
+      {
+        id: '1',
+        title: 'Santa tell me',
+        singer: '아리아나 그란데',
+        thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkiyQB1J-DtEEa4EoflA3XqEoWe6VuBqdTw&usqp=CAU',
+        audioSource: '',
+      },
+      {
+        id: '2',
+        title: '미리 메리 크리스마스',
+        singer: '아이유',
+        thumbnail: 'http://news.imaeil.com/inc/photos/2019/05/03/2019050311475036152_l.jpg',
+        audioSource: '',
+      },
+      {
+        id: '1',
+        title: 'Santa tell me',
+        singer: '아리아나 그란데',
+        thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkiyQB1J-DtEEa4EoflA3XqEoWe6VuBqdTw&usqp=CAU',
+        audioSource: '',
+      },
+      {
+        id: '2',
+        title: '미리 메리 크리스마스',
+        singer: '아이유',
+        thumbnail: 'http://news.imaeil.com/inc/photos/2019/05/03/2019050311475036152_l.jpg',
+        audioSource: '',
+      },
     ]);
   };
 
--- src/pages/PlayerPage.tsx
+++ src/pages/PlayerPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PageTemplate from '../components/common/PageTemplate';
 import Player from '../components/Player';
+import MusicList from '../components/Playlist/MusicList';
 
 const PlaylistPage = (): JSX.Element => {
   return (
     <PageTemplate>
       <Player />
+      <MusicList />
     </PageTemplate>
   );
 };
