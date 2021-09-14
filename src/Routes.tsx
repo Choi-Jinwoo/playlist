ffdcfd8d9e4715b69f31782771f31e8dbdf19da9
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-14 09:10:36 +0900

    fix: PlaylistPage를 PlayerPage로 변경

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import PlaylistPage from './pages/PlaylistPage';
+import PlayerPage from './pages/PlayerPage';
 
 const Routes = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/playlist/:id" component={PlaylistPage} />
+      <Route exact path="/player/:id" component={PlayerPage} />
     </BrowserRouter>
   );
 };
