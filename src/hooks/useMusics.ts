c3709cf71fc9d1d9487f6f6d6eb2dc681a0ba7cc
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-14 17:56:25 +0900

    fix: useMusic의 useEffect을 분리하여 infitie useEffect 오류 수정

--- src/hooks/useMusics.ts
+++ src/hooks/useMusics.ts
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { select } from '../actions/music';
 import { Music } from '../models/music';
 
 type UseMusics = [Music[]];
 
 const useMusics = (): UseMusics => {
   const [musics, setMusics] = useState<Music[]>([]);
+  const dispatch = useDispatch();
 
   const handleFetch = async () => {
     setMusics([
@@ -13,7 +16,7 @@ const useMusics = (): UseMusics => {
         title: 'Santa tell me',
         singer: '아리아나 그란데',
         thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkiyQB1J-DtEEa4EoflA3XqEoWe6VuBqdTw&usqp=CAU',
-        audioSource: '',
+        audioSource: '/music/santa-tell-me.mp3',
       },
       {
         id: '2',
@@ -23,28 +26,28 @@ const useMusics = (): UseMusics => {
         audioSource: '',
       },
       {
-        id: '1',
+        id: '3',
         title: 'Santa tell me',
         singer: '아리아나 그란데',
         thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkiyQB1J-DtEEa4EoflA3XqEoWe6VuBqdTw&usqp=CAU',
         audioSource: '',
       },
       {
-        id: '2',
+        id: '4',
         title: '미리 메리 크리스마스',
         singer: '아이유',
         thumbnail: 'http://news.imaeil.com/inc/photos/2019/05/03/2019050311475036152_l.jpg',
         audioSource: '',
       },
       {
-        id: '1',
+        id: '5',
         title: 'Santa tell me',
         singer: '아리아나 그란데',
         thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkiyQB1J-DtEEa4EoflA3XqEoWe6VuBqdTw&usqp=CAU',
         audioSource: '',
       },
       {
-        id: '2',
+        id: '6',
         title: '미리 메리 크리스마스',
         singer: '아이유',
         thumbnail: 'http://news.imaeil.com/inc/photos/2019/05/03/2019050311475036152_l.jpg',
@@ -56,6 +59,12 @@ const useMusics = (): UseMusics => {
   useEffect(() => {
     handleFetch();
   }, []);
+
+  useEffect(() => {
+    if (musics.length >= 0) {
+      dispatch(select(musics[0]));
+    }
+  }, [dispatch, musics]);
 
   return [musics];
 };
