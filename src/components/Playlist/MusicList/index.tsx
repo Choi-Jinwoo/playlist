5e5e32bf5b9acbe254be0e9b54e7f2c196e058ba
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-14 10:12:54 +0900

    add: MusicList에 title 추가

--- src/components/Playlist/MusicList/index.tsx
+++ src/components/Playlist/MusicList/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import useMusics from '../../../hooks/useMusics';
+import theme from '../../../styles/theme';
+import Text from '../../common/Text';
 import MusicItem from '../MusicItem';
 
 const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+`;
 
+const ListWrapper = styled.div`
+  width: 100%;
+  height: 350px;
+  overflow: auto;
+  margin-top: 40px;
 `;
 
 const MusicList = (): JSX.Element => {
@@ -14,7 +27,10 @@ const MusicList = (): JSX.Element => {
 
   return (
     <Container>
-      {MusicItems}
+      <Text weight='bold' size={theme.fontSize.xLarge} color={theme.color.main1}>playlist</Text>
+      <ListWrapper>
+        {MusicItems}
+      </ListWrapper>
     </Container>
   );
 };
