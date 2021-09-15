a8396c6b9ff7759158cc1bc5992668bdccefb53b
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-15 21:05:30 +0900

    add: Player의  Header 컴포넌트 추가

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FaList } from 'react-icons/fa';
+import DirectoryList from '../Home/Directory/DirectoryList';
+import useModal from '../../hooks/useModal';
+
+const Container = styled.header`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const MenuListWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > * {
+    margin-right: 10px;
+
+    &:last-child {
+      margin-right: 0px;
+    }
+  }
+`;
+
+const DirectoriesButton = styled(FaList)`
+  cursor: pointer;
+  color: ${props => props.theme.color.main1};
+
+`;
+
+const DirectoryListWrapper = styled.div`
+  position: absolute;
+  left: 40px;
+  z-index: 10;
+  background-color: ${props => props.theme.color.black}90;
+  border-radius: 8px;
+  padding: 20px 60px;
+  height: 95%;
+  overflow: auto;
+
+  & > * {
+    width: ${props => props.theme.innerWidth.desktop};
+  }
+`;
+
+const Header = (): JSX.Element => {
+  const [isDirectoriesOpen, handleOpenDirectories] = useModal(false, 'directory-list-wrapper');
+
+  return (
+    <Container>
+      <MenuListWrapper>
+        <DirectoriesButton onClick={handleOpenDirectories} />
+      </MenuListWrapper>
+
+      {isDirectoriesOpen &&
+        <DirectoryListWrapper className="directory-list-wrapper">
+          <DirectoryList />
+        </DirectoryListWrapper>
+      }
+    </Container>
+  )
+}
+
+export default Header;
--- src/pages/PlayerPage.tsx
+++ src/pages/PlayerPage.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import PageTemplate from '../components/common/PageTemplate';
 import Player from '../components/Player';
 import MusicList from '../components/Playlist/MusicList';
+import Header from '../components/Header';
 
 const PlaylistPage = (): JSX.Element => {
   return (
     <PageTemplate>
+      <Header />
       <Player />
       <MusicList />
     </PageTemplate>
