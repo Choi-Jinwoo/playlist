ce5c19b384902b881b6773ebee9f340e277db65d
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-15 19:38:24 +0900

    add: data.json을 통해 directory 및 music 사용

--- src/hooks/useDirectories.ts
+++ src/hooks/useDirectories.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Directory } from '../models/directory';
+import data from '../assets/data/data.json';
 
 type UseDirectories = [Directory[]];
 
@@ -7,32 +8,7 @@ const useDirectories = (): UseDirectories => {
   const [directories, setDirectories] = useState<Directory[]>([]);
 
   const handleFetch = async () => {
-    setDirectories([
-      {
-        id: '1',
-        title: '팝송 List',
-        description: '감성적인 팝송 모음',
-        thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkiyQB1J-DtEEa4EoflA3XqEoWe6VuBqdTw&usqp=CAU',
-      },
-      {
-        id: '2',
-        title: '김주엽 노래',
-        description: '김주엽이 추천하는 노래 모음',
-        thumbnail: 'http://news.imaeil.com/inc/photos/2019/05/03/2019050311475036152_l.jpg',
-      },
-      {
-        id: '3',
-        title: '팝송 List',
-        description: '감성적인 팝송 모음',
-        thumbnail: 'https://img1.daumcdn.net/thumb/R1280x0.fpng/?fname=http://t1.daumcdn.net/brunch/service/user/8fXh/image/M7kpICX4bbcXfuMgX2DH0NT0qeg.png',
-      },
-      {
-        id: '4',
-        title: '팝송 List',
-        description: '감성적인 팝송 모음',
-        thumbnail: '',
-      },
-    ]);
+    setDirectories(data.directories);
   };
 
   useEffect(() => {
--- src/hooks/useMusics.ts
+++ src/hooks/useMusics.ts
@@ -1,60 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { initMusics, selectMusic } from '../actions/music';
 import { Music } from '../models/music';
 import { RootState } from '../reducers';
+import data from '../assets/data/data.json';
+import { Directory } from '../models/directory';
 
 type UseMusics = [Music[] | null];
 
-const useMusics = (): UseMusics => {
+const useMusics = (directoryId: string): UseMusics => {
   const musics = useSelector((state: RootState) => state.music.musics);
   const dispatch = useDispatch();
 
   const handleFetch = async () => {
-    dispatch(initMusics([
-      {
-        id: '1',
-        title: 'Santa tell me',
-        singer: '아리아나 그란데',
-        thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkiyQB1J-DtEEa4EoflA3XqEoWe6VuBqdTw&usqp=CAU',
-        audioSource: '/music/santa-tell-me.mp3',
-      },
-      {
-        id: '2',
-        title: '미리 메리 크리스마스',
-        singer: '아이유',
-        thumbnail: 'http://news.imaeil.com/inc/photos/2019/05/03/2019050311475036152_l.jpg',
-        audioSource: '/music/merry-christmas.mp3',
-      },
-      {
-        id: '3',
-        title: 'Santa tell me',
-        singer: '아리아나 그란데',
-        thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkiyQB1J-DtEEa4EoflA3XqEoWe6VuBqdTw&usqp=CAU',
-        audioSource: '',
-      },
-      {
-        id: '4',
-        title: '미리 메리 크리스마스',
-        singer: '아이유',
-        thumbnail: 'http://news.imaeil.com/inc/photos/2019/05/03/2019050311475036152_l.jpg',
-        audioSource: '',
-      },
-      {
-        id: '5',
-        title: 'Santa tell me',
-        singer: '아리아나 그란데',
-        thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkiyQB1J-DtEEa4EoflA3XqEoWe6VuBqdTw&usqp=CAU',
-        audioSource: '',
-      },
-      {
-        id: '6',
-        title: '미리 메리 크리스마스',
-        singer: '아이유',
-        thumbnail: 'http://news.imaeil.com/inc/photos/2019/05/03/2019050311475036152_l.jpg',
-        audioSource: '',
-      },
-    ]));
+    const directory = data.directories.find((directory: Directory) => directory.id === directoryId);
+    if (directory === undefined) return;
+    dispatch(initMusics(directory.musics));
   };
 
   useEffect(() => {
