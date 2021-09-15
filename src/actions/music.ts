fbe99beecb1ea579c8bbaafe1651c0641c0c8032
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-15 17:11:45 +0900

    add: NextMusic, PreviousMusic 관련 Action 추가

--- src/actions/music.ts
+++ src/actions/music.ts
@@ -2,6 +2,8 @@ import { BaseAction } from '.';
 import { Music } from '../models/music';
 
 export const SELECT_MUSIC = 'SELECT_MUSIC';
+export const TO_NEXT_MUSIC = 'TO_NEXT_MUSIC';
+export const TO_PREVIOUS_MUSIC = 'TO_PREVIOUS_MUSIC';
 
 export type MusicAction = SelectAction;
 
@@ -9,7 +11,23 @@ type SelectAction = {
   music: Music,
 } & BaseAction;
 
+type ToNextAction = {
+  // empty
+} & BaseAction;
+
+type ToPreviousAction = {
+  // empty
+} & BaseAction;
+
 export const select = (music: Music): SelectAction => ({
   type: SELECT_MUSIC,
   music,
+});
+
+export const toNext = (): ToNextAction => ({
+  type: TO_NEXT_MUSIC,
+});
+
+export const toPrevious = (): ToPreviousAction => ({
+  type: TO_PREVIOUS_MUSIC,
 });
