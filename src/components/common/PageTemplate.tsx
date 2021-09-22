dbaf3e4f49eb5fbb6134f8593a8f9f4dd49b48ce
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-09-22 21:57:00 +0900

    add: PageTemplate Contents의 inner width 반응형 추가

--- src/components/common/PageTemplate.tsx
+++ src/components/common/PageTemplate.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import theme from '../../styles/theme';
+import { composeMediaQuery } from '../../utils/css';
 
 const Container = styled.div``;
 
@@ -7,6 +9,11 @@ const Contents = styled.div`
   width: ${props => props.theme.innerWidth.desktop};
   padding: 20px 0px;
   margin: 0 auto;
+
+  ${composeMediaQuery({
+    tablet: `width: ${theme.innerWidth.tablet};`,
+    mobile: `width: ${theme.innerWidth.mobile}`,
+  })}
 `;
 
 type Props = {
